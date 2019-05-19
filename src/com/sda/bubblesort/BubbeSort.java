828583eb0f7f59c3231712e266f18303c7c73c82
valentyna <nezabudka3107@gmail.com>
2019-05-19 16:02:35 +0200

    BubbleSort

---
+++ src/com/sda/bubblesort/BubbeSort.java
@@ -0,0 +1,37 @@
+package com.sda.bubblesort;
+
+import java.util.Random;
+
+public class BubbeSort {
+    static int arrSize = 20, upperBound = 26;
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[arrSize];
+
+        Random rand = new Random();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(upperBound) - 5;
+            //System.out.print(" " + numbers[i]);
+            int[] sorteArr = sort(arr);
+        }
+    }
+
+    private static int[] sort(int[] arr) {
+        boolean isSorted = true;
+
+        while (!isSorted) {
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = arr[i];
+                    isSorted = false;
+
+                }
+            }
+        }
+        return arr;
+    }
+}
---
+++ src/com/sda/primenumber/PrimeSelector.java
@@ -0,0 +1,17 @@
+package com.sda.primenumber;
+
+import java.util.Scanner;
+
+public class PrimeSelector {
+    public boolean isPrime(int n) {
+        int i = n - 1;
+        while (i > 1) {
+            if (n % i == 0)
+                return false;
+            --i;
+        }
+        return true;
+
+    }
+
+}
