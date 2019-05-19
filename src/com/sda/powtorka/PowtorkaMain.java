088d91f5e6f506f16d13f1bf20d74c076de0bf45
valentyna <nezabudka3107@gmail.com>
2019-05-19 10:55:47 +0200

    Rectangle  & Square

---
+++ src/com/sda/powtorka/PowtorkaMain.java
@@ -0,0 +1,31 @@
+package com.sda.powtorka;
+
+import java.util.Scanner;
+
+public class PowtorkaMain {
+    public static void main(String[] args) {
+        System.out.println("To write rectangle type 1");
+        System.out.println("To write square type 2");
+        System.out.println("To write traaingle type 3");
+
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        switch (number){
+            case 1:
+                System.out.println("Plese enter lenght od side A");
+                int sideA = scanner.nextInt();
+                System.out.println("Plese enter lenght od side B");
+                int sideB = scanner.nextInt();
+                Rectangle rec = new Rectangle(sideA, sideB);
+                rec.draw();
+                break;
+            case 2:
+                System.out.println("Plese enter lenght od side");
+                int side = scanner.nextInt();
+                Square square = new Square(side);
+                square.draw();
+                break;
+        }
+    }
+}
